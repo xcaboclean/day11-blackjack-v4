c05ed6fc0dcb6bfda139e7d1a3db024163ab3b97
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2024-01-25 23:46:15 +0000

    new feat - class Deck

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from art import logo
-
+import random
 class Card:
   def __init__(self, rank, suit):
     self.rank = rank
     self.suit = suit
 
+class Deck:
+  def __init__(self):
+      ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+      suits = ["♠️", "♣️", "♥️", "♦️", "♠️", "♣️", "♥️", "♦️"]
+      self.cards = [Card(rank, suit) for rank in ranks for suit in suits]
+      random.shuffle(self.cards)
+    
+  def display_size_deck(self):
+    print(f"\n┌───────────────┐\n│{len(self.cards):03}🃏🎴 card(s)│\n└───────────────┘")
+
 class BlackJackGame:
+  def __init__(self):
+    self.deck = Deck()
+
   def play(self):
     print(logo)
     
